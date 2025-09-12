78c9913daf2c34b94d2fca086f758746a623e417
Bodrug Pavel <plkoij73@gmail.com>
2025-09-13 00:41:16 +0300

    task one done

--- task1/src/main/java/Main.java
+++ task1/src/main/java/Main.java
@@ -1,5 +1,60 @@
-public class Main {
-    public static void main(String[] args) {
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
 
+public class Main {
+
+    private static final int MAX = 25;
+    private static final Lock LOCK = new ReentrantLock();
+    private static final Condition ODD = LOCK.newCondition();
+    private static final Condition EVEN = LOCK.newCondition();
+
+    private static volatile int counter = 0;
+
+    public static void main(String[] args) {
+        Thread odd = new Thread(printer("Odd Thread", true));
+        Thread even = new Thread(printer("Even Thread", false));
+
+        odd.start();
+        even.start();
+    }
+
+    private static Runnable printer(String name, boolean isOdd) {
+        return () -> {
+            while (true) {
+                LOCK.lock();
+                try {
+                    Condition current = isOdd ? ODD : EVEN;
+                    Condition other = isOdd ? EVEN : ODD;
+
+                    while (counter <= MAX && isOdd() != isOdd) {
+                        current.await();
+                    }
+                    if (counter > MAX) {
+                        other.signal();
+                        return;
+                    }
+
+                    print(name);
+                    counter++;
+
+                    other.signal();
+
+                    Thread.sleep(500);
+                } catch (InterruptedException e) {
+                    Thread.currentThread().interrupt();
+                } finally {
+                    LOCK.unlock();
+                }
+            }
+        };
+    }
+
+    private static boolean isOdd() {
+        return counter % 2 != 0;
+    }
+
+    private static void print(String name) {
+        System.out.println(name + " writes the number " + counter);
     }
 }
