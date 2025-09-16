1a6465742a2671aac715e06b23584375c4dc968f
Bodrug Pavel <plkoij73@gmail.com>
2025-09-16 17:41:48 +0300

    task 2 done

--- part2/src/main/java/MainTask2.java
+++ part2/src/main/java/MainTask2.java
@@ -1,5 +1,35 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 public class MainTask2 {
     public static void main(String[] args) {
+        try (ServerSocket serverSocket = new ServerSocket(8080)) {
+            while (true) {
+                Socket client = serverSocket.accept();
+                new Thread(() -> handle(client)).start();
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
 
+    private static void handle(Socket socket) {
+        try (BufferedReader input = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+             PrintWriter output = new PrintWriter(socket.getOutputStream(), true)) {
+            String line;
+
+            while ((line = input.readLine()) != null && !line.equalsIgnoreCase("exit")) {
+                System.out.println("Echo: " + line);
+                output.println(line + "\r\n");
+            }
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
+
