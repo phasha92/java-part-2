c44e26628fb0bc9902af0f998c0fed28929ca16b
Bodrug Pavel <plkoij73@gmail.com>
2025-10-01 02:01:43 +0300

    fixed  added classes:   EchoServer.java   Client.java  edit: Main logic

--- part2/src/main/java/MainTask2.java
+++ part2/src/main/java/MainTask2.java
@@ -1,35 +1,18 @@
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.PrintWriter;
-import java.net.ServerSocket;
-import java.net.Socket;
 
 public class MainTask2 {
-    public static void main(String[] args) {
-        try (ServerSocket serverSocket = new ServerSocket(8080)) {
-            while (true) {
-                Socket client = serverSocket.accept();
-                new Thread(() -> handle(client)).start();
-            }
-        } catch (IOException e) {
-            throw new RuntimeException(e);
+    public static void main(String[] args) throws IOException, InterruptedException {
+
+        try (EchoServer server = new EchoServer();
+             Client client1 = new Client("Vasya");
+             Client client2 = new Client("Petya")) {
+            Thread serverThread = new Thread(server);
+            serverThread.start();
+            if (client1.connect("", 8080)) new Thread(client1).start();
+            if (client2.connect("localhost", 8080)) new Thread(client2).start();
+            serverThread.join();
         }
-    }
 
-    private static void handle(Socket socket) {
-        try (BufferedReader input = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-             PrintWriter output = new PrintWriter(socket.getOutputStream(), true)) {
-            String line;
-
-            while ((line = input.readLine()) != null && !line.equalsIgnoreCase("exit")) {
-                System.out.println("Echo: " + line);
-                output.println(line + "\r\n");
-            }
-
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
     }
 }
 
